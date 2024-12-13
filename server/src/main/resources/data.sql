6384e030d7afb4e8e9264d2a5be533ed609310ea
Gaston Pascal tonguino <pgastong03@gmail.com>
2024-12-13 02:14:34 +0000

    add resources files

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (name, category, quantity, price) VALUES
+('Laptop', 'Electronic', 15, 999.99),
+('Smartphone', 'Electronic', 30, 699.99),
+('Printer', 'Office', 10, 150.00),
+('Mechanic Keyboard', 'Accessories', 20, 75.50),
+('Optical Mouse', 'Accessories', 25, 25.00),
+('Headphone', 'Audio', 12, 120.00),
+('TV LED', 'Electronic', 5, 499.99),
+('Touchscreen Tablet', 'Electronic', 18, 299.99),
+('External hard disk', 'Storage', 22, 89.99),
+('Universal Charger', 'Accessories', 50, 19.99);
---
+++ server/src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL
+);
