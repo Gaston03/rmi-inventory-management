563e662bcc88e39d75275e59b39ac27ce628613b
Gaston Pascal tonguino <pgastong03@gmail.com>
2024-12-16 01:23:46 +0000

    minor changes

--- server/src/main/java/ma/supmti/Main.java
+++ server/src/main/java/ma/supmti/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             /*
-            *  Initialize database by creating [Products] and [Users] databases
+            *  Initialize database by creating [Products] and [Users] tables
             * */
             DatabaseConnection.initializeDatabase();
             ProductService productService = new ProductServiceImpl();
